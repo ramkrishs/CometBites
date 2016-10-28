f10c70c800e2fb086f02b38467fd84528d7cd703
twinklesharma1311 <twinklesharma1311@gmail.com>
2016-10-27 19:29:27 -0500

    Sign up page.

---
+++ app/src/main/java/edu/utdallas/cometbites/PhoneVerifyActivity.java
@@ -0,0 +1,13 @@
+package edu.utdallas.cometbites;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PhoneVerifyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phone_verify);
+    }
+}
